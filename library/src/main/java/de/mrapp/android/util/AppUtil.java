11421052ece6961895710c9cccee7df7578bd1b3
Michael Rapp <michael.rapp90@googlemail.com>
2017-01-03 21:16:43 +0100

    Method arguments of the type Context are now used instead of arguments of the type Activity, whenever possible.

--- library/src/main/java/de/mrapp/android/util/AppUtil.java
+++ library/src/main/java/de/mrapp/android/util/AppUtil.java
@@ -16,6 +16,7 @@ package de.mrapp.android.util;
 import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -120,55 +121,54 @@ public final class AppUtil {
      * Starts the gallery app in order to show a specific image. If an error occurs while starting
      * the gallery app, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the gallery app should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the gallery app should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param file
      *         The file, which stores the image, which should be shown, as an instance of the class
      *         {@link File}. The file may not be null. The file must exist and must not be a
      *         directory.
      */
-    public static void startGalleryApp(@NonNull final Activity activity, @NonNull final File file) {
+    public static void startGalleryApp(@NonNull final Context context, @NonNull final File file) {
         ensureNotNull(file, "The file may not be null");
         ensureFileIsNoDirectory(file, "The file must exist and must not be a directory");
-        startGalleryApp(activity, Uri.parse("file://" + file.getAbsolutePath()));
+        startGalleryApp(context, Uri.parse("file://" + file.getAbsolutePath()));
     }
 
     /**
      * Starts the gallery app in order to show a specific image. If an error occurs while starting
      * the gallery app, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the gallery app should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the gallery app should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param uri
      *         The URI of the image, which should be shown, as an instance of the class {@link Uri}.
      *         The URI may not be null and must be valid
      */
-    public static void startGalleryApp(@NonNull final Activity activity, @NonNull final Uri uri) {
-        ensureNotNull(activity, "The activity may not be null");
+    public static void startGalleryApp(@NonNull final Context context, @NonNull final Uri uri) {
+        ensureNotNull(context, "The context may not be null");
         ensureNotNull(uri, "The URI may not be null");
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(uri, "image/*");
-        activity.startActivity(intent);
+        context.startActivity(intent);
     }
 
     /**
      * Starts the web browser in order to show a specific URI. If an error occurs while starting the
      * web browser an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the web browser should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the web browser should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param uri
      *         The URI, which should be shown, as a {@link String}. The URI may neither be null, nor
      *         empty
      */
-    public static void startWebBrowser(@NonNull final Activity activity,
-                                       @NonNull final String uri) {
+    public static void startWebBrowser(@NonNull final Context context, @NonNull final String uri) {
         ensureNotNull(uri, "The URI may not be null");
         ensureNotEmpty(uri, "The URI may not be empty");
-        startWebBrowser(activity, Uri.parse(
+        startWebBrowser(context, Uri.parse(
                 (uri.startsWith("http://") || uri.startsWith("https://")) ? uri :
                         ("http://" + uri)));
     }
@@ -177,43 +177,43 @@ public final class AppUtil {
      * Starts the web browser in order to show a specific URI. If an error occurs while starting the
      * web browser an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the web browser should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the web browser should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param uri
      *         The URI, which should be shown, as an instance of the class {@link Uri}. The URI may
      *         not be null
      */
-    public static void startWebBrowser(@NonNull final Activity activity, @NonNull final Uri uri) {
-        ensureNotNull(activity, "The activity may not be null");
+    public static void startWebBrowser(@NonNull final Context context, @NonNull final Uri uri) {
+        ensureNotNull(context, "The context may not be null");
         ensureNotNull(uri, "The URI may not be null");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        activity.startActivity(intent);
+        context.startActivity(intent);
     }
 
     /**
      * Starts the mail client in order to send an e-mail to a specific e-mail address. If an error
      * occurs while starting the mail client, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the mail client should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the mail client should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param emailAddress
      *         The e-mail address, the e-mail should be sent to, as a {@link String}. The e-mail
      *         address may neither be null, nor empty
      */
-    public static void startMailClient(@NonNull final Activity activity,
+    public static void startMailClient(@NonNull final Context context,
                                        @NonNull final String emailAddress) {
-        startMailClient(activity, emailAddress, null, null);
+        startMailClient(context, emailAddress, null, null);
     }
 
     /**
      * Starts the mail client in order to send an e-mail to a specific e-mail address. If an error
      * occurs while starting the mail client, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the mail client should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the mail client should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param emailAddress
      *         The e-mail address, the e-mail should be sent to, as a {@link String}. The e-mail
      *         address may neither be null, nor empty
@@ -224,38 +224,38 @@ public final class AppUtil {
      *         The text of the e-mail, which should be sent, as a {@link String}  or null, if the
      *         e-mail should not have a text
      */
-    public static void startMailClient(@NonNull final Activity activity,
+    public static void startMailClient(@NonNull final Context context,
                                        @NonNull final String emailAddress,
                                        @Nullable final String subject,
                                        @Nullable final String text) {
         ensureNotNull(emailAddress, "The e-mail address may not be null");
         ensureNotEmpty(emailAddress, "The e-mail address may not be empty");
-        startMailClient(activity, new String[]{emailAddress}, subject, text);
+        startMailClient(context, new String[]{emailAddress}, subject, text);
     }
 
     /**
      * Starts the mail client in order to send an e-mail to specific e-mail addresses. If an error
      * occurs while starting the mail client, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the mail client should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the mail client should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param emailAddresses
      *         An array, which contains the e-mail addresses, the e-mail should be sent to, as a
      *         {@link String} array. The array may neither be null, nor empty
      */
-    public static void startMailClient(@NonNull final Activity activity,
+    public static void startMailClient(@NonNull final Context context,
                                        @NonNull final String[] emailAddresses) {
-        startMailClient(activity, emailAddresses, null, null);
+        startMailClient(context, emailAddresses, null, null);
     }
 
     /**
      * Starts the mail client in order to send an e-mail to specific e-mail addresses. If an error
      * occurs while starting the mail client, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the mail client should be started from, as an instance of the class
-     *         {@link Activity}. The activity may not be null
+     * @param context
+     *         The context, the mail client should be started from, as an instance of the class
+     *         {@link Context}. The context may not be null
      * @param emailAddresses
      *         An array, which contains the e-mail addresses, the e-mail should be sent to, as a
      *         {@link String} array. The array may neither be null, nor empty
@@ -266,11 +266,11 @@ public final class AppUtil {
      *         The text of the e-mail, which should be sent, as a {@link String}  or null, if the
      *         e-mail should not have a text
      */
-    public static void startMailClient(@NonNull final Activity activity,
+    public static void startMailClient(@NonNull final Context context,
                                        @NonNull final String[] emailAddresses,
                                        @Nullable final String subject,
                                        @Nullable final String text) {
-        ensureNotNull(activity, "The activity may not be null");
+        ensureNotNull(context, "The context may not be null");
         ensureNotNull(emailAddresses, "The array may not be null");
         ensureTrue(emailAddresses.length > 0, "The array may not be empty");
         Intent intent = new Intent(Intent.ACTION_SEND);
@@ -285,7 +285,7 @@ public final class AppUtil {
             intent.putExtra(Intent.EXTRA_TEXT, text);
         }
 
-        activity.startActivity(intent);
+        context.startActivity(intent);
     }
 
     /**
@@ -293,14 +293,14 @@ public final class AppUtil {
      * started by the user. If an error occurs while starting the dialer, an {@link
      * ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be dialed, as an {@link Integer} value
      */
-    public static void startDialer(@NonNull final Activity activity, final int phoneNumber) {
-        startDialer(activity, Integer.toString(phoneNumber));
+    public static void startDialer(@NonNull final Context context, final int phoneNumber) {
+        startDialer(context, Integer.toString(phoneNumber));
     }
 
     /**
@@ -308,14 +308,14 @@ public final class AppUtil {
      * started by the user. If an error occurs while starting the dialer, an {@link
      * ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be dialed, as a {@link Long} value
      */
-    public static void startDialer(@NonNull final Activity activity, final long phoneNumber) {
-        startDialer(activity, Long.toString(phoneNumber));
+    public static void startDialer(@NonNull final Context context, final long phoneNumber) {
+        startDialer(context, Long.toString(phoneNumber));
     }
 
     /**
@@ -323,21 +323,21 @@ public final class AppUtil {
      * started by the user. If an error occurs while starting the dialer, an {@link
      * ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be dialed, as a {@link String}. The phone number may
      *         neither be null, nor empty
      */
-    public static void startDialer(@NonNull final Activity activity,
+    public static void startDialer(@NonNull final Context context,
                                    @NonNull final String phoneNumber) {
-        ensureNotNull(activity, "The activity may not be null");
+        ensureNotNull(context, "The context may not be null");
         ensureNotNull(phoneNumber, "The phone number may not be null");
         ensureNotEmpty(phoneNumber, "The phone number may not be empty");
         Uri uri = Uri.parse(phoneNumber.startsWith("tel:") ? phoneNumber : "tel:" + phoneNumber);
         Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-        activity.startActivity(intent);
+        context.startActivity(intent);
     }
 
     /**
@@ -346,15 +346,15 @@ public final class AppUtil {
      * granted, otherwise a {@link SecurityException} will be thrown. If an error occurs while
      * starting the dialer, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be called, as an {@link Integer} value
      */
     @RequiresPermission(Manifest.permission.CALL_PHONE)
-    public static void startCall(@NonNull final Activity activity, final int phoneNumber) {
-        startDialer(activity, Integer.toString(phoneNumber));
+    public static void startCall(@NonNull final Context context, final int phoneNumber) {
+        startDialer(context, Integer.toString(phoneNumber));
     }
 
     /**
@@ -363,15 +363,15 @@ public final class AppUtil {
      * granted, otherwise a {@link SecurityException} will be thrown. If an error occurs while
      * starting the dialer, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be called, as a {@link Long} value
      */
     @RequiresPermission(Manifest.permission.CALL_PHONE)
-    public static void startCall(@NonNull final Activity activity, final long phoneNumber) {
-        startDialer(activity, Long.toString(phoneNumber));
+    public static void startCall(@NonNull final Context context, final long phoneNumber) {
+        startDialer(context, Long.toString(phoneNumber));
     }
 
     /**
@@ -380,22 +380,22 @@ public final class AppUtil {
      * granted, otherwise a {@link SecurityException} will be thrown. If an error occurs while
      * starting the dialer, an {@link ActivityNotFoundException} will be thrown.
      *
-     * @param activity
-     *         The activity, the dialer should be started from, as an instance of the class {@link
-     *         Activity}. The activity may not be null
+     * @param context
+     *         The context, the dialer should be started from, as an instance of the class {@link
+     *         Context}. The context may not be null
      * @param phoneNumber
      *         The phone number, which should be called, as a {@link String}. The phone number may
      *         neither be null, nor empty
      */
     @RequiresPermission(Manifest.permission.CALL_PHONE)
-    public static void startCall(@NonNull final Activity activity,
+    public static void startCall(@NonNull final Context context,
                                  @NonNull final String phoneNumber) {
-        ensureNotNull(activity, "The activity may not be null");
+        ensureNotNull(context, "The context may not be null");
         ensureNotNull(phoneNumber, "The phone number may not be null");
         ensureNotEmpty(phoneNumber, "The phone number may not be empty");
         Uri uri = Uri.parse(phoneNumber.startsWith("tel:") ? phoneNumber : "tel:" + phoneNumber);
         Intent intent = new Intent(Intent.ACTION_CALL, uri);
-        activity.startActivity(intent);
+        context.startActivity(intent);
     }
 
 }
