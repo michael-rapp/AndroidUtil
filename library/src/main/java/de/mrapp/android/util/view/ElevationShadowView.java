6f0ee48163020d991e177f749ce1b3f40ac832c3
Michael Rapp <michael.rapp90@googlemail.com>
2017-03-22 22:07:35 +0100

    The class ElevationShadowView is now extended from the class AppCompatImageView.

--- library/src/main/java/de/mrapp/android/util/view/ElevationShadowView.java
+++ library/src/main/java/de/mrapp/android/util/view/ElevationShadowView.java
@@ -13,16 +13,14 @@
  */
 package de.mrapp.android.util.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Build;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
 import de.mrapp.android.util.ElevationUtil;
 import de.mrapp.android.util.ElevationUtil.Orientation;
@@ -40,7 +38,7 @@ import static de.mrapp.android.util.ElevationUtil.createElevationShadow;
  * @author Michael Rapp
  * @since 1.2.2
  */
-public class ElevationShadowView extends ImageView {
+public class ElevationShadowView extends AppCompatImageView {
 
     /**
      * The elevation of the shadow, which is visualized by the view, in dp.
@@ -216,34 +214,6 @@ public class ElevationShadowView extends ImageView {
                                @AttrRes final int defaultStyle) {
         super(context, attributeSet, defaultStyle);
         initialize(attributeSet, defaultStyle, 0);
-    }
-
-    /**
-     * Creates a new view, which can be used to visualize the shadow of an elevation on pre-Lollipop
-     * devices.
-     *
-     * @param context
-     *         The context, which should be used by the view, as an instance of the class {@link
-     *         Context}. The context may not be null
-     * @param attributeSet
-     *         The attribute set, the view's attributes should be obtained from, as an instance of
-     *         the type {@link AttributeSet} or null, if no attributes should be obtained
-     * @param defaultStyle
-     *         The default style to apply to this view. If 0, no style will be applied (beyond what
-     *         is included in the theme). This may either be an attribute resource, whose value will
-     *         be retrieved from the current theme, or an explicit style resource
-     * @param defaultStyleResource
-     *         A resource identifier of a style resource that supplies default values for the view,
-     *         used only if the default style is 0 or can not be found in the theme. Can be 0 to not
-     *         look for defaults
-     */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public ElevationShadowView(@NonNull final Context context,
-                               @Nullable final AttributeSet attributeSet,
-                               @AttrRes final int defaultStyle,
-                               @StyleRes final int defaultStyleResource) {
-        super(context, attributeSet, defaultStyle, defaultStyleResource);
-        initialize(attributeSet, defaultStyle, defaultStyleResource);
     }
 
     /**
